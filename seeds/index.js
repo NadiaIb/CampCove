c9aba4b19259f31b7f0d8f619f0b90b51abaea1d
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2024-07-02 13:09:41 +0100

    Add images - reseed data

--- seeds/index.js
+++ seeds/index.js
@@ -11,7 +11,8 @@ async function main() {
   console.log("Database connected");
 }
 
-const sample = (array) => { //returns random value in array for seedHelpers
+const sample = (array) => {
+  //returns random value in array for seedHelpers
   return array[Math.floor(Math.random() * array.length)];
 };
 
@@ -20,14 +21,19 @@ const seedDB = async () => {
   await campGround.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000); // random num used to index cities array
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new campGround({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`, // calling sample function and passing descriptors/places array to pick out random values from both arrays,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur incidunt odio rem, velit voluptatum aliquid quasi sint est sit vel porro accusamus magnam suscipit vero rerum consequatur necessitatibus maiores nobis",
+      price,
     });
     await camp.save();
   }
 };
 
-seedDB().then(()=>{
-  mongoose.connection.close()
-})
+seedDB().then(() => {
+  mongoose.connection.close();
+});
